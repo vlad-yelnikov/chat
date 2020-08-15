fb023be113d887687d3bf8dcda2c6cd2fabec08b
vlad-yelnikov <vlad.yelnikov@gmail.com>
2020-08-15 21:37:21 +0300

    install eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    node: true,
+    commonjs: true,
+    es2020: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 11,
+  },
+  rules: {},
+};
